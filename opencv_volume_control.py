c804e48cc9642e7314448141163b5d87b8b33728
Debayan <pradhandebayan@gmail.com>
2022-02-15 15:46:05 +0530

    made the video optically inverted to match real time directions

--- opencv_volume_control.py
+++ opencv_volume_control.py
@@ -6,6 +6,7 @@ from pycaw.pycaw import AudioUtilities , IAudioEndpointVolume
 from comtypes import CLSCTX_ALL
 from ctypes import cast, POINTER
 
+
 vid = cv2.VideoCapture(0)
 vid.set(3,800)
 vid.set(4,800)
@@ -22,37 +23,41 @@ ran = volume.GetVolumeRange()
 min = ran[0]
 max = ran[1]
 
-
 while 1:
     var, img = vid.read()
-    imgrgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    hflip = cv2.flip(img, 1)
+    imgrgb = cv2.cvtColor(hflip, cv2.COLOR_BGR2RGB)
     res = hands.process(imgrgb)
     lst = []
     
     if res.multi_hand_landmarks:
         for i in  res.multi_hand_landmarks:
             for id,val in enumerate(i.landmark):
-                h,w,c = img.shape
+                h,w,c = hflip.shape
                 cx,cy = int(val.x*w), int(val.y*h)
                 lst.append([id,cx,cy])
-            mpdraw.draw_landmarks(img,i,mphands.HAND_CONNECTIONS)
+            mpdraw.draw_landmarks(hflip,i,mphands.HAND_CONNECTIONS)
 
     if len(lst):
         x1,y1 = lst[4][1],lst[4][2]
         x2,y2 = lst[8][1],lst[8][2]
-        xm = (x1+x2)/2
-        ym = (y1+y2)/2
 
-        cv2.circle(img,(x1,y1),10,(0,0,0),cv2.FILLED)
-        cv2.circle(img,(x2,y2),10,(0,0,0),cv2.FILLED)
-        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
+        xm = (x1+x2)//2
+        ym = (y1+y2)//2
+
+        cv2.circle(hflip,(x1,y1),10,(0,0,0),cv2.FILLED)
+        cv2.circle(hflip,(x2,y2),10,(0,0,0),cv2.FILLED)
+        cv2.circle(hflip,(xm,ym),5,(0,0,0),cv2.FILLED)
+        cv2.line(hflip,(x1,y1),(x2,y2),(255,0,255),3)
 
         length = math.hypot(x2-x1,y2-y1)
-        print(length)
+        # print(length)
+
         vol = np.interp(length,[18,150],[min,max])
         volume.SetMasterVolumeLevel(vol, None)
+       
 
-    cv2.imshow("webcam",img)
+    cv2.imshow("webcam",hflip)
     key = cv2.waitKey(1)
     if key == 113:
         break

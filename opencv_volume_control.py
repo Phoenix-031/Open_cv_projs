d57afd8ae6e6903b499e9e2cd795498dbf2e7b05
Debayan <pradhandebayan@gmail.com>
2022-02-15 09:12:10 +0530

    changed teh volume parameters

--- opencv_volume_control.py
+++ opencv_volume_control.py
@@ -18,8 +18,10 @@ mpdraw = mp.solutions.drawing_utils
 dev = AudioUtilities.GetSpeakers()
 inter = dev.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(inter, POINTER(IAudioEndpointVolume))
+ran = volume.GetVolumeRange()
+min = ran[0]
+max = ran[1]
 
-min,max = volume.GetVolumeRange()[:2]
 
 while 1:
     var, img = vid.read()
@@ -30,24 +32,26 @@ while 1:
     if res.multi_hand_landmarks:
         for i in  res.multi_hand_landmarks:
             for id,val in enumerate(i.landmark):
-                # print(id,val)
                 h,w,c = img.shape
                 cx,cy = int(val.x*w), int(val.y*h)
                 lst.append([id,cx,cy])
             mpdraw.draw_landmarks(img,i,mphands.HAND_CONNECTIONS)
+
     if len(lst):
         x1,y1 = lst[4][1],lst[4][2]
         x2,y2 = lst[8][1],lst[8][2]
+        xm = (x1+x2)/2
+        ym = (y1+y2)/2
 
         cv2.circle(img,(x1,y1),10,(0,0,0),cv2.FILLED)
         cv2.circle(img,(x2,y2),10,(0,0,0),cv2.FILLED)
         cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
 
         length = math.hypot(x2-x1,y2-y1)
-        vol = np.interp(length,[15,220],[min,max])
+        print(length)
+        vol = np.interp(length,[18,150],[min,max])
         volume.SetMasterVolumeLevel(vol, None)
-        
-        
+
     cv2.imshow("webcam",img)
     key = cv2.waitKey(1)
     if key == 113:
